e7d618ce514de335bbec8524e59ef361015c078c
David Silva <david.silva@processmind.com.br>
2021-08-01 13:08:16 -0300

    core simple script

---
+++ core/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##
+## Este arquivo realiza a build do frontend do projeto e da play no backend
+## Talvez possamos manter as regras do diretório _frontend e no futuro remover ele do repositório
+## Assim funcionará o 'deploy' do mesmo jeito para qualquer aplicação
+##
+
+echo "Iniciando build do frontend"
+cd ../_frontend
+npm install
+npm run build
+cd ..
+
+echo "Iniciando build do backend"
+cd backend
+npm install
+npm start
+cd ..
+echo "Build finalizada"
