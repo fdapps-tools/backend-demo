14f7ddfc823ff6306ffeac0d694c2c59c55521c1
David Silva <david.silva@processmind.com.br>
2021-08-11 19:52:38 -0300

    new routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path')
+const nodeController = require('../controllers/nodeController')
 
-// GET / - Retorna o frontend compilado
-router.get('/', (req, res, next) => {
-  return res.sendFile(path.join(__dirname + '/../../frontend/index.html'))
-});
+router.get('/', (req, res, next) => res.sendFile(path.join(__dirname + '/../../frontend/index.html')));
 
-// @todo -> Retornar o binário completo que ainda não existe
-router.get('/download', (req, res, next) => {});
+router.get('/nodes', nodeController.index);
 
-// @todo -> Retornar a lista com todos os nós ativos da rede
-router.get('/nodes', (req, res, next) => {});
+router.post('/join', nodeController.store);
 
 // GET /stats - Retorna o link do tunel
-router.get('/stats', (req, res, next) => {
-  const tunnel = req.app.get('tunnel')
-  return res.send({ url: tunnel })
-});
+router.get('/stats', (req, res, next) => res.send({ url: req.app.get('tunnel') }));
+
+// @todo -> Retornar o binário completo que ainda não existe
+router.get('/download', (req, res, next) => { });
 
 module.exports = router;
