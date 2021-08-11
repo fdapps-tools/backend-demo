1b0433d5fe9ec6c66395d30fa20a1b28c188173b
David Silva <david.silva@processmind.com.br>
2021-08-11 16:37:16 -0300

    localtunnel settings

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,5 +13,10 @@ router.get('/download', (req, res, next) => {});
 // @todo -> Retornar a lista com todos os nós ativos da rede
 router.get('/nodes', (req, res, next) => {});
 
+// GET /stats - Retorna o link do tunel
+router.get('/stats', (req, res, next) => {
+  const tunnel = req.app.get('tunnel')
+  return res.send({ url: tunnel })
+});
 
 module.exports = router;
