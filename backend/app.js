b69116a209bc5486acaa24313c9f434c2d2c7847
David Silva <david.silva@processmind.com.br>
2021-08-12 19:08:31 -0300

    crontab and organizing bin

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cron = require('node-cron');
+const localtunnel = require('localtunnel');
+const nodeRepository = require('./repositories/nodeRepository')
 
-var indexRouter = require('./routes/index');
+require('dotenv').config()
+
+cron.schedule('* * * * *', () => nodeRepository.syncNodes())
+
+async function startLocalTunnel() {
+  const tunnel = await localtunnel({ port: process.env.PORT || '61635' });
+
+  app.set('tunnel', tunnel.url);
+  console.log(`tunnel running: ${tunnel.url}`)
+
+  console.log('setting as node...')
+  await nodeRepository.insertNode({ host: tunnel.url, lastcheck: Date.now() })
+  console.log('setted as node!')
+
+  tunnel.on('close', async () => {
+    // @todo: pensar em uma estratégia para reconexão
+    console.log(`tunnel closed, trying reconnect`)
+    startLocalTunnel()
+  });
+  return tunnel
+}
+
+startLocalTunnel();
+
+const indexRouter = require('./routes/index');
 
 var app = express();
 
