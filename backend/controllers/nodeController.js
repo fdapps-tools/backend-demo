79cab0071705aa7c6bbd53109b77f3606f274099
David Silva <david.silva@processmind.com.br>
2021-08-14 16:15:25 -0300

    remove store node controller method

--- backend/controllers/nodeController.js
+++ backend/controllers/nodeController.js
@@ -5,31 +5,15 @@ class nodeController {
   async index(req, res) {
 
     try {
-
       const list = await nodeRepository.getNodeList()
       // @todo: implementar um middlware para cors
       res.setHeader('Access-Control-Allow-Origin', '*');
       return res.json(list)
-
     } catch (error) {
       console.log('ERROR', error)
       return res.status(500).send({ error: 'Something failed!' })
     }
 
-  }
-
-  async store(req, res) {
-    try {
-
-      const node = { host: req.app.get('tunnel'), lastcheck: Date.now() }
-      const inserted = await nodeRepository.insertNode(node)
-
-      return res.json(inserted)
-
-    } catch (error) {
-      console.log('ERROR', error)
-      return res.status(500).send({ error: 'Something failed!' })
-    }
   }
 
   async joinRequest(req, res) {
