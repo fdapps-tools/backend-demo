b2eafb07da556b31741b702b405ea49b694844f5
David Silva <david.silva@processmind.com.br>
2021-08-14 16:15:35 -0300

    fix cron lib

--- backend/libs/cron.js
+++ backend/libs/cron.js
@@ -1,8 +1,9 @@
 const cron = require('node-cron');
+const nodeRepository = require('../repositories/nodeRepository');
 
 module.exports = {
   initCron(tunnelUrl) {
-
+    console.log('initCron')
     cron.schedule('* * * * *', () => nodeRepository.checkNodesIsUp());
     cron.schedule('* * * * *', () => nodeRepository.syncJoinRequests(tunnelUrl));
     
