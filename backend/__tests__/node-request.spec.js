691f9c4442a5a87775a7876ef6c55542d67d967e
David Silva <david.silva@processmind.com.br>
2021-08-20 17:39:31 -0300

    check only property

--- backend/__tests__/node-request.spec.js
+++ backend/__tests__/node-request.spec.js
@@ -4,12 +4,8 @@ describe('libs/node-request', () => {
 
   it('ensure node-request instance has bypass custom header', async () => {
     const { headers } = instance().defaults
-
-    expect(headers).toEqual(expect.objectContaining({
-        "Bypass-Tunnel-Reminder": expect.any(Boolean),
-        ...headers
-      }
-    ));
+    
+    expect(headers).toHaveProperty('Bypass-Tunnel-Reminder')
   });
 
 })
