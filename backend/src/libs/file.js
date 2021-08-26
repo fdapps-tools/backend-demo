2153f0993e339bf8eb93b05f8f8f1f75b903c2e0
David Silva <david.silva@processmind.com.br>
2021-08-26 19:11:53 -0300

    check file

--- backend/src/libs/file.js
+++ backend/src/libs/file.js
@@ -1,17 +1,26 @@
-
 const fs = require('fs').promises;
 const PATH = './src/localDB'
 
 module.exports = {
 
   async getFile(filename) {
-    const data = await fs.readFile(`${PATH}/${filename}.state`, 'utf8')
-    return data ? JSON.parse(data) : []
+    let data = "[]"
+
+    try {
+      data = await fs.readFile(`${PATH}/${filename}.state`, 'utf8')
+
+    } catch (error) {
+      if (error.errno == "-2") {
+        await fs.writeFile(`${PATH}/${filename}.state`, "[]")
+      }
+    }
+
+    return JSON.parse(data)
   },
 
   async updateFile(data, filename) {
-    await fs.writeFile(`${PATH}/${filename}.state`, JSON.stringify(data))
-    return true
+      await fs.writeFile(`${PATH}/${filename}.state`, JSON.stringify(data))
+      return true
   },
-  
+
 }
