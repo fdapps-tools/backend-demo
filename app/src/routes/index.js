76693d806ccc5899b8e84e07ab427922e777b94d
David Silva <david.silva@processmind.com.br>
2021-11-22 18:58:52 -0300

    remove routes

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const nodeController = require('../controllers/nodeController')
+const nodeManager = require('node-manager')
 
+/**
+ * Route to delivery static frontend
+ */
 router.get('/', ({ res }) => res.sendFile(require('path').resolve('./') + '/frontend/'));
 
-router.get('/nodes', nodeController.index);
-router.post('/join-request', nodeController.joinRequest);
-router.post('/update-node-info', nodeController.updateNodeInfo);
-
-router.get('/stats', (req, res, next) => res.send({ url: process.env.TUNNEL_URL }));
+/**
+ * Inject fdApps routes on express router
+ * Routes: GET /starts, GET /nodes, POST /join-request, POST /update-node-info
+ */
+nodeManager.attachRoutes(router)
 
 module.exports = router;
